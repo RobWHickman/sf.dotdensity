986c7b3e8e438fcf7f7dcd6c955ac8cc14231748
RobWHickman <robwhickman@gmail.com>
2020-01-01 16:23:30 +0000

    added bosnian data

---
+++ R/bosnia_ethnicity_data.R
@@ -0,0 +1,34 @@
+#' Census Ethnicity Data to replicate Bosnia dot density map from https://twitter.com/ImerM1/status/1037358973807210498
+#'
+#' A dataset containing the ethnicity data for Bosnian districts in
+#'  the 2013 census
+#'
+#' \itemize{
+#'   \item{district}{The name of the district for the 2013 Bosnian census}
+#'   \item{Total}{The number of people of all ethnicities in the 2013 Bosnian census}
+#'   \item{Bosniak}{The number of people of Bosniak ethnicity in the 2013 Bosnian census}
+#'   \item{Serb}{The number of people of Serb ethnicity in the 2013 Bosnian census}
+#'   \item{Croat}{The number of people of Croat ethnicity in the 2013 Bosnian census}
+#'   \item{Bosnian}{The number of people of Bosnian ethnicity in the 2013 Bosnian census}
+#'   \item{Romani}{The number of people of Romani ethnicity in the 2013 Bosnian census}
+#'   \item{Muslim}{The number of people of Muslim ethnicity in the 2013 Bosnian census}
+#'   \item{Bosnian.and.Herzegovinian}{The number of people of Bosnian and Herzegovinian ethnicity in the 2013 Bosnian census}
+#'   \item{Albanian}{The number of people of Albanian ethnicity in the 2013 Bosnian census}
+#'   \item{Yugoslav}{The number of people of Yugoslav ethnicity in the 2013 Bosnian census}
+#'   \item{Ukranian}{The number of people of Ukranian ethnicity in the 2013 Bosnian census}
+#'   \item{Montenegrin}{The number of people of Montenegrin ethnicity in the 2013 Bosnian census}
+#'   \item{Turk}{The number of people of Turkish ethnicity in the 2013 Bosnian census}
+#'   \item{Slovenian}{The number of people of Slovenian ethnicity in the 2013 Bosnian census}
+#'   \item{Orthodox}{The number of people of Orthodox ethnicity in the 2013 Bosnian census}
+#'   \item{Macedonian}{The number of people of Macedonian ethnicity in the 2013 Bosnian census}
+#'   \item{Others}{The number of people of Other ethnicity in the 2013 Bosnian census}
+#'   \item{Undeclared}{The number of people who did not declare ethnicity in the 2013 Bosnian census}
+#'   \item{Unknown}{The number of people of Unknown ethnicity in the 2013 Bosnian census}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name london_election_data
+#' @usage data(london_election_data)
+#' @format A data frame with 141 rows and 20 variables
+"bosnia_ethnicity_data"
---
+++ R/bosnia_shapefile.R
@@ -0,0 +1,16 @@
+#' Shape Data to replicate Bosnia dot density map from https://twitter.com/ImerM1/status/1037358973807210498
+#'
+#' A dataset containing the simple features shapefile for Bosnia & Herzegovina
+#'     districts for the 2013 census. Downloaded from gadm.org
+#'
+#' \itemize{
+#'   \item{district}{The name of the district for the 2013 Bosnian census}
+#'   \item{geometery}{The geometry of the shapefile}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name london_shapefile
+#' @usage data(london_shapefile)
+#' @format A data frame with 141 rows and 2 variables
+"bosnia_shapefile"
